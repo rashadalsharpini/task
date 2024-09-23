8d9331af9cdb004d52ae12f5416ff2726071d354
rashad <rashadalsharpini@gmail.com>
2024-09-23 13:39:41 +0300

    reorganized code and added the search and sort function

--- ConsoleApp2/ClassLibrary2/obj/Debug/net8.0/ClassLibrary2.AssemblyInfo.cs
+++ ConsoleApp2/ClassLibrary2/obj/Debug/net8.0/ClassLibrary2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibrary2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7c241f938c9bc434cf1fc9062e0e8cb94e3029c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibrary2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLibrary2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,120 +1,186 @@
 ﻿using System;
+using System.Linq;
 using ClassLibrary1;
 
 namespace day3
 {
-    class task2
+    class Task2
     {
         static void Main(string[] args)
         {
-            string[] menu = { "New", "Display", "Exit" };
+            string[] menu = { "New", "Display", "Search", "Sort", "Exit" };
             int width = Console.WindowWidth / 2;
             int height = Console.WindowHeight / (menu.Length + 1);
-            int Highlight = 0;
-            bool flag = true;
-            Class1.Employee[] array = null;
+            int highlight = 0;
+            bool isRunning = true;
+            Class1.Employee[] employees = null;
 
-            do
+            while (isRunning)
             {
-                Console.Clear();
-                for (int i = 0; i < menu.Length; i++)
-                {
-                    if (Highlight == i)
-                    {
-                        Console.BackgroundColor = ConsoleColor.Green;
-                    }
-                    else
-                    {
-                        Console.BackgroundColor = ConsoleColor.Black;
-                    }
+                DisplayMenu(menu, width, height, highlight);
 
-                    Console.SetCursorPosition(width, (i + 1) * height);
-                    Console.Write(menu[i]);
-                    Console.ResetColor();
-                }
-
-                ConsoleKeyInfo info = Console.ReadKey();
-                switch (info.Key)
+                var keyPressed = Console.ReadKey().Key;
+                switch (keyPressed)
                 {
                     case ConsoleKey.UpArrow:
-                        Highlight--;
-                        if (Highlight < 0) Highlight = menu.Length - 1;
+                        highlight = (highlight > 0) ? highlight - 1 : menu.Length - 1;
                         break;
                     case ConsoleKey.DownArrow:
-                        Highlight++;
-                        if (Highlight >= menu.Length) Highlight = 0;
+                        highlight = (highlight < menu.Length - 1) ? highlight + 1 : 0;
                         break;
                     case ConsoleKey.Home:
-                        Highlight = 0;
+                        highlight = 0;
                         break;
                     case ConsoleKey.End:
-                        Highlight = menu.Length - 1;
+                        highlight = menu.Length - 1;
                         break;
                     case ConsoleKey.Enter:
-                        switch (Highlight)
-                        {
-                            case 0: // New 
-                                Console.Clear();
-                                Console.WriteLine("Enter the number of Employees you want to register:");
-                                int size = int.Parse(Console.ReadLine());
-                                array = new Class1.Employee[size];
-
-                                for (int i = 0; i < array.Length; i++)
-                                {
-                                    Console.Clear();
-                                    Console.WriteLine($"Enter the details for Employee {i + 1}:");
-                                    array[i] = Class1.Employee.ReadInfo();
-                                    Console.WriteLine($"The info of Employee {i + 1} was registered successfully.");
-                                }
-
-                                Console.WriteLine("Enter any key to return to the main menu.");
-                                Console.ReadLine();
-                                break;
-
-                            case 1: // Display 
-                                Console.Clear();
-                                if (array != null)
-                                {
-                                    // Sort employees by name in descending order
-                                    // array = array.OrderByDescending(e => e.Name).ToArray();
-                                    
-                                    // Sort employees by name in ascending order
-                                    array = array.OrderBy(e => e.Name).ToArray();
-                                    
-                                    // Sort employees by salary in descending order
-                                    // array = array.OrderByDescending(e => e.Salary).ToArray();
-                                    
-                                    //sort employees by salary in ascending order 
-                                    // array = array.OrderBy(e => e.Salary).ToArray();
-                                    
-                                    //Sort employees by ege in descending order
-                                    //array = array.OrderByDescending(e => e.Age).ToArray();
-                                    
-                                    //Sort employees by ege in ascending order
-                                    //array = array.OrderBy(e => e.Age).ToArray(); 
-                                    foreach (var employee in array)
-                                    {
-                                        employee.DisplayData();
-                                        Console.WriteLine();
-                                    }
-                                }
-                                else
-                                {
-                                    Console.WriteLine("No employee data available.");
-                                }
-
-                                Console.WriteLine("Enter any key to return to the main menu.");
-                                Console.ReadLine();
-                                break;
-
-                            case 2: // Exit
-                                flag = false;
-                                break;
-                        }
-
+                        HandleMenuSelection(menu[highlight], ref employees, ref isRunning);
                         break;
                 }
-            } while (flag);
+            }
+        }
+
+        static void DisplayMenu(string[] menu, int width, int height, int highlight)
+        {
+            Console.Clear();
+            for (int i = 0; i < menu.Length; i++)
+            {
+                Console.BackgroundColor = (i == highlight) ? ConsoleColor.Green : ConsoleColor.Black;
+                Console.SetCursorPosition(width, (i + 1) * height);
+                Console.Write(menu[i]);
+                Console.ResetColor();
+            }
+        }
+
+        static void HandleMenuSelection(string selectedOption, ref Class1.Employee[] employees, ref bool isRunning)
+        {
+            switch (selectedOption)
+            {
+                case "New":
+                    RegisterEmployees(ref employees);
+                    break;
+                case "Display":
+                    DisplayEmployees(employees);
+                    break;
+                case "Search":
+                    SearchEmployee(employees);
+                    break;
+                case "Sort":
+                    SortEmployees(ref employees);
+                    break;
+                case "Exit":
+                    isRunning = false;
+                    break;
+            }
+        }
+
+        static void RegisterEmployees(ref Class1.Employee[] employees)
+        {
+            Console.Clear();
+            Console.WriteLine("Enter the number of Employees you want to register:");
+            int size = int.Parse(Console.ReadLine());
+            employees = new Class1.Employee[size];
+
+            for (int i = 0; i < employees.Length; i++)
+            {
+                Console.Clear();
+                Console.WriteLine($"Enter the details for Employee {i + 1}:");
+                employees[i] = Class1.Employee.ReadInfo();
+                Console.WriteLine($"The info of Employee {i + 1} was registered successfully.");
+            }
+
+            Console.WriteLine("Enter any key to return to the main menu.");
+            Console.ReadLine();
+        }
+
+        static void DisplayEmployees(Class1.Employee[] employees)
+        {
+            Console.Clear();
+            if (employees != null && employees.Length > 0)
+            {
+                foreach (var employee in employees)
+                {
+                    employee.DisplayData();
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("No employee data available.");
+            }
+
+            Console.WriteLine("Enter any key to return to the main menu.");
+            Console.ReadLine();
+        }
+
+        static void SearchEmployee(Class1.Employee[] employees)
+        {
+            if (employees == null || employees.Length == 0)
+            {
+                Console.Clear();
+                Console.WriteLine("No employees available to search.");
+                Console.ReadLine();
+                return;
+            }
+
+            Console.Clear();
+            Console.WriteLine("Search by (1) ID or (2) Name:");
+            int choice = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the search value:");
+            string search = Console.ReadLine();
+            bool found = false;
+
+            foreach (var employee in employees)
+            {
+                if ((choice == 1 && employee.ID == int.Parse(search)) || (choice == 2 && employee.Name == search))
+                {
+                    Console.WriteLine("Employee found:");
+                    employee.DisplayData();
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found)
+            {
+                Console.WriteLine("No employee found with the given criteria.");
+            }
+
+            Console.WriteLine("Enter any key to return to the main menu.");
+            Console.ReadLine();
+        }
+
+        static void SortEmployees(ref Class1.Employee[] employees)
+        {
+            if (employees == null || employees.Length == 0)
+            {
+                Console.Clear();
+                Console.WriteLine("No employees available to sort.");
+                Console.ReadLine();
+                return;
+            }
+
+            Console.Clear();
+            Console.WriteLine("Sort by: (1) Name, (2) Salary, (3) Age:");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    employees = employees.OrderBy(e => e.Name).ToArray();
+                    break;
+                case 2:
+                    employees = employees.OrderBy(e => e.Salary).ToArray();
+                    break;
+                case 3:
+                    employees = employees.OrderBy(e => e.Age).ToArray();
+                    break;
+            }
+
+            Console.WriteLine("Employees sorted successfully.");
+            Console.WriteLine("Enter any key to return to the main menu.");
+            Console.ReadLine();
         }
     }
 }
